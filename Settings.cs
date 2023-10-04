3f0db7bdfaf75f9506c25c818904f6329b95b551
ADDB <addb1234567890@gmail.com>
2023-10-04 23:57:33 +0200

    Change default config

--- Settings.cs
+++ Settings.cs
@@ -14,7 +14,7 @@ namespace ForceOffline {
             doForceOfflineMode = ForceOffline.conf.Bind("", "doForceOfflineMode", true, new ConfigDescription("Force the game to start in offline mode"));
             CopyOnlineEvenIfExistingInProgressOffline = ForceOffline.conf.Bind("", "CopyOnlineEvenIfExistingInProgressOffline", false, new ConfigDescription("Set to true to always copy the latest online data" +
                 " even if there is an in-progress offline run (which would be deleted)"));
-            ReportOnline = ForceOffline.conf.Bind("", "ReportOnline", true, new ConfigDescription("When this is disabled, offline runs are kept separately in a way that they won't be reported" +
+            ReportOnline = ForceOffline.conf.Bind("", "ReportOnline", false, new ConfigDescription("When this is disabled, offline runs are kept separately in a way that they won't be reported" +
                 " after going back online. This is to prevent any kind of data/synchronization/verification problems. If you're playing with cheats/game changing mods this should be enabled!"));
         }
     }
